50a01cb0ebd4a0b73df9b5b384d3839b40ef5e4e
Hernan Tapia <soliditize@MacBook-Pro.local>
2023-02-25 15:58:04 -0800

    added contact form page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,34 @@
 import React from "react";
 
 function Contact () {
-    return  <div className="container"> 
+    return  <div className="container mt-5"> 
                 <h1>Contact Me!</h1>
 
-                <form>
-                    <label>Name: </label>
-                    <input type="text" name="name"/>
-
-                    <label>Email: </label>
-                    <input type="email" name="user_email"/>
-
-                    <label>Message: </label>
-                    <textarea name="message" rows="4"/>
-                    <input type="submit" value='send'/>
+                <form className="row g-3">
+                    <div className="col-md-6"> 
+                        <label for="firstName" className="form-label">First Name: </label>
+                        <input type="text" className="form-control" id="firstName" required/>
+                    </div>
+                    <div className="col-md-6">
+                        <label for="lastName" className="form-label">Last Name: </label>
+                        <input type="text" className="form-control" id="lastName" required/>
+                    </div>
+                    <div className="col-md-8">
+                        <label for="emailInfo" className="form-label">Email: </label>
+                        <input type="email" className="form-control" id="emailInfo" required></input>
+                    </div>
+                    <div className="col-md-4">
+                        <label for="phoneNumber" className="form-label">Phone Number</label>
+                        <input type="text" className="form-control" id="phoneNumber"></input>
+                    </div>
+                    <div className="col-md-12">
+                        <label for="message" className="form-label">Ask a question</label>
+                        <textarea className="form-control" id="message" rows="4"></textarea>
+                    </div>
+                    <div className="col-md-12">
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
                 </form>
-
             </div>
 }
 
