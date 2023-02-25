da3fb388a1cf15c914c38e2b9289abcb6d34432f
Hernan Tapia <soliditize@MacBook-Pro.local>
2023-02-25 13:18:08 -0800

    Form added, need stylying

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,21 @@
 import React from "react";
 
-
 function Contact () {
-    return  <div className> Contact
+    return  <div className="container"> 
+                <h1>Contact Me!</h1>
+
+                <form>
+                    <label>Name: </label>
+                    <input type="text" name="name"/>
+
+                    <label>Email: </label>
+                    <input type="email" name="user_email"/>
+
+                    <label>Message: </label>
+                    <textarea name="message" rows="4"/>
+                    <input type="submit" value='send'/>
+                </form>
+
             </div>
 }
 
